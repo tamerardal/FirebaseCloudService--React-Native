57e64a9520a3aee355979d5a68ad86cd5e2856db
Tamer <47778933+tamerardal@users.noreply.github.com>
2023-01-09 13:11:51 +0300

    final commit.

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import messaging from '@react-native-firebase/messaging';
+
+const App = () => {
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      messaging().getToken().then(token => {
+        console.log(token);
+      })
+    } else {
+      console.log("Failed token status. ", authStatus);
+    }
+
+    messaging().getInitialNotification().then(async remoteMessage => {
+      if (remoteMessage) {
+        console.log("Notification caused app to open from quit state:", remoteMessage.notification,);        
+      }
+    });
+
+    messaging().onNotificationOpenedApp(async remoteMessage => {
+      console.log("Notification caused app to open from background state:", remoteMessage.notification,);
+    });
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+
+    const unsubcribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert(remoteMessage.notification.title, remoteMessage.notification.body);
+    });
+
+    return unsubcribe;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Firebase Cloud Messaging Client using React Native.</Text>
     </View>
   );
 }
@@ -18,3 +61,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
